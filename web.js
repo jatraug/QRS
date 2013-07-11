b5d4124948ce019db7dc562923e0f654d9f48740
Jim Traugott <ubuntu@ip-10-245-74-160.us-west-2.compute.internal>
2013-07-11 02:08:11 +0000

    Changed buf.toString

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');	
-	var ind = fs.readFileSync('index.html');
-	var buf= new Buffer(ind, 'utf8');
-	response.send(buf.toString);
-//  response.send('Hello World Two!');
+    var fs = require('fs');	
+    var ind = fs.readFileSync('index.html');
+    var buf= new Buffer(ind, 'utf8');
+    response.send(buf.toString());
+    //  response.send('Hello World Two!');
 });
 
 var port = process.env.PORT || 5000;
